62b2341e6384ab431243d5550c596fd998ebb808
Jesus Jimenez <gesusjl@gmail.com>
2021-12-03 00:15:55 +0100

    exercise 1.5 step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -20,15 +20,25 @@ const Content = (props) => {
   )
 }
 
-const Total = (props) => (
+const Total = (props) => {
+  console.log(props)
+  return (
 
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>
+    Number of exercises {
+        props.parts[0].exercises + 
+        props.parts[1].exercises + 
+        props.parts[2].exercises 
+      }
+  </p>)
 
-)
+}
 
 const App = () => {
-  const course = "Half stack application development"
-  const parts = [
+ 
+  const course = {
+    name : "Half stack application development",
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -41,18 +51,18 @@ const App = () => {
       name: 'State of a component',
       exercises: 14
     }
-
-  ] 
+  ]
+} 
 
   return (
 
     <div>
 
-      <Header course={course} />
+      <Header course={course.name} />
       
-      <Content parts = {parts}/>
+      <Content parts = {course.parts} />
 
-      <Total parts = {parts}/>
+      <Total parts = {course.parts} />
 
     </div>
 
