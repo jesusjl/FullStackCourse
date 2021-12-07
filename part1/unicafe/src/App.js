c740ab258e0643408f08869ab77a2981beedd24c
Jesus Jimenez <gesusjl@gmail.com>
2021-12-07 23:57:08 +0100

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,30 @@
 import React, {useState} from 'react';
 
+const Button = (props) => {
+
+  return <button onClick={props.onClick}> {props.text} </button>
+
+}
+
+const StatisticLine = ({text, value}) => {
+  return <tr><td>{text}</td><td> {value}</td></tr>
+}
+
 const Statistics = (props) => {
-  console.log(props)
+  if(props.value['all'] === 0) {
+    return <p>No feedback given</p>
+  }
   return (
-    <p>{props.text} {props.feedback}</p>
+    <table>
+    <tbody>
+    <StatisticLine text='good' value={props.value['good']}/>
+    <StatisticLine text='neutral' value={props.value['neutral']} />
+    <StatisticLine text='bad' value={props.value['bad']} />
+    <StatisticLine text='all' value={props.value['all']} />
+    <StatisticLine text='average' value= {props.value['average']} />
+    <StatisticLine text='positive' value= {props.value['positive']} />
+    </tbody>
+    </table>
   )
 }
 
@@ -13,23 +34,34 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)
+  const positive = ((good / (good + neutral + bad)) *100) + (' %')
+  
+  const value = {
+
+    'good': good,
+    'neutral': neutral, 
+    'bad': bad, 
+    'all': all,
+    'average': average,
+    'positive': positive
+
+  }
+
   return (
+
     <div>
 
       <h2>give feedback</h2>
-
-      <button onClick={()=>setGood(good + 1)}>Good</button>
-      <button onClick={()=>setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={()=>setBad(bad + 1)}>Bad</button>
+      <Button  onClick={()=>setGood(good + 1)} text="Good"/>
+      <Button  onClick={()=>setNeutral(neutral + 1)} text="Neutral"/>
+      <Button  onClick={()=>setBad(bad + 1)} text="Bad"/>
 
       <h2>Statistics</h2>
 
-      <Statistics text='good' feedback={good} />
-      <Statistics text='neutral' feedback={neutral} />
-      <Statistics text='bad' feedback={bad} />
-      <Statistics text='average' feedback= {(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)} />
-      <Statistics text='positive' feedback= {((good / (good + neutral + bad)) *100) + (' %')} />
-      
+      <Statistics value={value}/>
+
     </div>
   )
 }
