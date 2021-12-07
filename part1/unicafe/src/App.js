a2374fbb27529ff33f65143a8afefe2d58d6353f
Jesus Jimenez <gesusjl@gmail.com>
2021-12-07 20:36:10 +0100

    1.7: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
 
+const Statistics = (props) => {
+  console.log(props)
+  return (
+    <p>{props.text} {props.feedback}</p>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
@@ -9,19 +16,20 @@ const App = () => {
   return (
     <div>
 
-    <h2>give feedback</h2>
+      <h2>give feedback</h2>
 
-     <button onClick={()=>setGood(good + 1)}>Good</button>
-     <button onClick={()=>setNeutral(neutral + 1)}>Neutral</button>
-     <button onClick={()=>setBad(bad + 1)}>Bad</button>
+      <button onClick={()=>setGood(good + 1)}>Good</button>
+      <button onClick={()=>setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={()=>setBad(bad + 1)}>Bad</button>
 
-    <h2>Statistics</h2>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)}</p>
-    <p>positive {(good/ (good + neutral + bad)) *100} </p>
+      <h2>Statistics</h2>
+
+      <Statistics text='good' feedback={good} />
+      <Statistics text='neutral' feedback={neutral} />
+      <Statistics text='bad' feedback={bad} />
+      <Statistics text='average' feedback= {(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)} />
+      <Statistics text='positive' feedback= {((good / (good + neutral + bad)) *100) + (' %')} />
+      
     </div>
   )
 }
