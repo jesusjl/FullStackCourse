2c464795f56a1855df004630bedbf36444bd9af7
Jesus Jimenez <gesusjl@gmail.com>
2021-12-10 13:45:25 +0100

    2.3*: Course information step8

--- part2/course/src/index.js
+++ part2/course/src/index.js
@@ -8,10 +8,11 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-  return(
-    <p>Number of exercises {sum}</p>
-  ) 
+
+  const sum = course.parts.reduce((total, course) => course.exercises + total, 0)
+
+  return <p>Number of exercises {sum}</p>
+  
 }
 
 const Part = (props) => {
@@ -35,7 +36,7 @@ const Course = ({course}) => {
     <div>
     <Header course={course} />
     <Content course={course}/ >
-
+    <Total course={course}/ >
     </div>
   )
 } 
