22dbcc25c2143018c96ada1fff3496a7af84c38b
Jesus Jimenez <gesusjl@gmail.com>
2022-01-08 18:55:53 +0100

    5.3: bloglist frontend, step3

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getAll =  () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const createBlogPost = async (newObject) => {
+  const config = {
+    headers: {Authorization: token},
+  }
+  const response = axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+export default { getAll, createBlogPost, setToken }
